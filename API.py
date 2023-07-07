ab1eeee8eef0cecf33b4e71cbd2940dcd1a826fa
overcrash <3681221+overcrash66@users.noreply.github.com>
2023-07-06 23:40:16 -0300

    Update API.py
    clean code

--- API.py
+++ API.py
@@ -4,13 +4,10 @@ from service import gen_text
 from fastapi import FastAPI
 app = FastAPI()
 @app.get("/chat")
-#@app.post("/chat")
 
 def hello(question = None):
-    #question = input("ask a question: ")
     if question is None:
         text = "ask me a question!"
-        #time.sleep(5)
     else:
         text = gen_text(question)
         text = text[0]['generated_text']
